a49a67748278674cdc3c4328c78dd91564269c07
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-19 02:58:13 -0300

    animation form

--- src/components/Forms/FormLogin/index.js
+++ src/components/Forms/FormLogin/index.js
@@ -3,11 +3,15 @@ function FormLogin () {
       
       <div className="form">
       <div className="formLogin">
-        <h3>Login</h3>
+        <h3>Welcome</h3>
         <form onSubmit={submitLogin}>
             <input type="mail" placeholder="e-mail"/>
             <input type="password" placeholder="password"/>
-            <button type = 'submit'>Access</button>
+            <button type = 'submit'>Login</button>
+            <ul>
+                <li><a href="/About">Register</a></li>
+                <li><a href="/Mentors">Reset Password</a></li>
+            </ul>
         </form>
         </div>
       </div>
