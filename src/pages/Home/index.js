87e02e5f7c55917fe72655b5fc06bb7b21505c88
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-20 04:04:55 -0300

    adjusting border radius

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,11 @@
 function Home() {
   return (
     <div className="Home">
-      <a href="/Mentors"><button className="buttonStandard">Find a Mentor</button></a>
-      <h1> First Page</h1>
+      <div className="bannerHome">
+        <h1> First Page</h1>
+        <a href="/Mentors"><button className="buttonStandard">Find a Mentor</button></a>
+      </div>
+      
     </div>
   );
 }
