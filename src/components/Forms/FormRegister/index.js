0c4ed84b2888fad6939607ee6c9ad9e0594d56dd
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-26 20:22:57 -0300

    users

---
+++ src/components/Forms/FormRegister/index.js
@@ -0,0 +1,29 @@
+function FormRegister () {
+  
+  return (  
+      
+      <div className="form">
+      <div className="formLogin">
+        <h3>New Users</h3>
+        <form onSubmit={submitRegister}>
+            <input className="input_name" type="text" placeholder="name"/>
+            <input className="input_newmail" type="email" placeholder="e-mail"/>
+            <input className="input_pass" type="password" placeholder="password"/>
+            <input className="input_confirmpass" type="password" placeholder="password"/>
+            <button className="buttonStandard btn_register" type = 'submit'>Register</button>
+            <ul>
+                <li><a className="linkStandard" href="/About">Login</a></li>
+            </ul>
+        </form>
+        </div>
+      </div>
+      
+    );
+
+    function submitRegister (event) {
+        event.preventDefault();
+        alert('New User!!!!')
+    }
+  };
+  
+  export default FormRegister;
