691b280d9005c21564b1a1fd97c4800c013b171c
Paul Pessoa <paulmspessoa@gmail.com>
2022-03-26 19:30:46 -0300

    change favicon

--- src/components/Forms/FormRegister/index.js
+++ src/components/Forms/FormRegister/index.js
@@ -1,7 +1,7 @@
 function FormRegister () {
     
   let name = document.querySelector('.input_name');
-  let mail = document.querySelector('.input_mail');
+  let newmail = document.querySelector('.input_newmail');
   let pass = document.querySelector('.input_pass');
   let confirmpass = document.querySelector('.input_confirmpass');
   let btn_register = document.querySelector('.btn_register');
@@ -12,9 +12,9 @@ function FormRegister () {
       <div className="form">
       <div className="formLogin">
         <h3>New User</h3>
-        <form onSubmit={submitLogin}>
+        <form onSubmit={submitRegister}>
             <input className="input_name" type="text" placeholder="name"/>
-            <input className="input_mail" type="mail" placeholder="e-mail"/>
+            <input className="input_newmail" type="email" placeholder="e-mail"/>
             <input className="input_pass" type="password" placeholder="password"/>
             <input className="input_confirmpass" type="password" placeholder="password"/>
             <button className="buttonStandard btn_register" type = 'submit'>Register</button>
@@ -27,9 +27,10 @@ function FormRegister () {
       
     );
 
-    function submitLogin (event) {
+    function submitRegister (event) {
         event.preventDefault();
-        alert('Hey dude!!!!')
+        alert('New User!!!!')
+        console.log((name).value, (newmail).textContent)
     }
   };
   
