f269e571d7e0d0168d669d4178b2c28474ae2cad
Paul Pessoa <paulmspessoa@gmail.com>
2023-05-20 23:24:43 -0300

    fix metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,21 @@ import { Inter } from 'next/font/google'
 import Providers from './providers'
 import Header from '../layout/Header'
 import Footer from '../layout/Footer'
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
-export const metadata = {
-  title: 'Menvo',
-  description: 'Volunteer Mentors',
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <title>Menvo</title>
+        <meta name="description" content="Menvo is a free mentoring plataform for students looking for their first professional opportunities." />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preload" href="/fonts/inter-var-latin.woff2" as="font" type="font/woff2" crossOrigin="anonymous" />
+        {/* Outros itens relevantes que você deseja adicionar ao <head> */}
+      </Head>
       <body className={inter.className}>
         <Providers >
           <Header/>
