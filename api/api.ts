505d125c27887d631c5652b794af28c2c7ef7380
Paul Pessoa <paulmspessoa@gmail.com>
2023-05-22 03:20:28 -0300

    config APIs

--- api/api.ts
+++ api/api.ts
@@ -1,1 +1,10 @@
-console.log("api")
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_SUPABASE_URL + "/rest/v1/",
+  headers: {
+    apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+});
+
+export default api;
