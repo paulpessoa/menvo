4ff9cb40503c53cdff2ee8df3ea88e8752751363
Paul Pessoa <paulmspessoa@gmail.com>
2023-07-28 01:38:20 -0300

    install and config i18n

---
+++ src/app/i18n/settings.js
@@ -0,0 +1,15 @@
+export const fallbackLng = 'pt'
+export const languages = [fallbackLng, 'en', 'es']
+export const defaultNS = 'translation'
+
+export function getOptions (lng = fallbackLng, ns = defaultNS) {
+  return {
+    // debug: true,
+    supportedLngs: languages,
+    fallbackLng,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns
+  }
+}
