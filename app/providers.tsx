e70edf7c3c174f390cf7b3615d6866dcfc0bfe28
Paul Pessoa <paulmspessoa@gmail.com>
2025-07-11 18:04:43 -0300

    thema light

--- app/providers.tsx
+++ app/providers.tsx
@@ -24,7 +24,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
         <AuthProvider>
           {children}
           <Toaster />
